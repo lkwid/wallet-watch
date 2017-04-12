48f758bf9cb35d7f30c1421374c2ae6231001ad4
Łukasz Widawski <lk.widawski@gmail.com>
2017-04-13 00:03:32 +0200

    new operation form added

--- src/main/java/lkwid/controller/OperationController.java
+++ src/main/java/lkwid/controller/OperationController.java
@@ -2,9 +2,12 @@ package lkwid.controller;
 
 import java.util.Collection;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lkwid.entity.Operation;
@@ -18,9 +21,16 @@ public class OperationController {
 	
 	@RequestMapping("/account")
 	public String showOperations(Model model) {
-		Collection<Operation> operations = operationService.showOperations();
+		Collection<Operation> operations = operationService.showOperations();		
+		model.addAttribute("operation", new Operation());
 		model.addAttribute("operations", operations);
 		return "account";
 	}
+	
+	@PostMapping(value = "/operation", params = "save")
+	public String saveOperation(@Valid Operation operation) {			
+		operationService.saveOperation(operation);
+		return "redirect:/account";
+	}	
 
 }
